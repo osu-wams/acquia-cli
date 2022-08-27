ff062fcdf2862cb684cd7b0abdb1bcbd1f7c88f1
Matthew Brabham <brabham.matthew@gmail.com>
2022-08-26 17:09:55 -0700

    Add logic to find the configuration project. Add logic to add environment variables to the config object.

--- bin/osu-acquia-cli.php
+++ bin/osu-acquia-cli.php
@@ -5,19 +5,24 @@ use OsuWams\Cli\AcquiaCli;
 use Robo\Robo;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Filesystem\Path;
 
 $pharPath = Phar::running(TRUE);
 if ($pharPath) {
+  $root = $pharPath;
   $autoloaderPath = "$pharPath/vendor/autoload.php";
 }
 else {
   if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    $root = dirname(__DIR__);
     $autoloaderPath = __DIR__ . '/vendor/autoload.php';
   }
   elseif (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    $root = dirname(__DIR__) . '/../..';
     $autoloaderPath = __DIR__ . '/../vendor/autoload.php';
   }
   elseif (file_exists(__DIR__ . '/../../autoload.php')) {
+    $root = dirname(__DIR__) . '/../../..';
     $autoloaderPath = __DIR__ . '/../../autoload.php';
   }
   else {
@@ -25,12 +30,35 @@ else {
   }
 }
 $classLoader = require $autoloaderPath;
-
+$environment = [];
+if (getenv('ACQUIACLI_KEY')) {
+  $environment['acquia']['key'] = getenv('ACQUIACLI_KEY');
+}
+if (getenv('ACQUIACLI_SECRET')) {
+  $environment['acquia']['secret'] = getenv('ACQUIACLI_SECRET');
+}
+// Get the file path for the local project config.
+$localConfig = join(DIRECTORY_SEPARATOR, [
+  Path::getDirectory(__DIR__),
+  'acquia-cli.yml',
+]);
+// Get the file path for the global config.
+$globalConfig = join(DIRECTORY_SEPARATOR, [
+  Path::getHomeDirectory(),
+  '.acquia/acquia-cli.yml',
+]);
 // Customization variables
 $input = new ArgvInput($argv);
 $output = new ConsoleOutput();
 $appVersion = trim(file_get_contents(dirname(__DIR__) . '/VERSION'));
-$config = Robo::createConfiguration(['acquia-cli.yml']);
+$config = Robo::createConfiguration([$globalConfig, $localConfig]);
+// Check to see if we have env variables and update our config object.
+// Environment variables should override any other variables loaded from config
+// files
+if (isset($environment['acquia']['key']) && isset($environment['acquia']['secret'])) {
+  $config->set('acquia.key', $environment['acquia']['key']);
+  $config->set('acquia.secret', $environment['acquia']['secret']);
+}
 $app = new AcquiaCli($config, $input, $output);
 $statusCode = $app->run($input, $output);
 exit($statusCode);
