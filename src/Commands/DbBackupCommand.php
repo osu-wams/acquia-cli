22659cbc2711dc71d9e3dfd9907ac7fe67ac57a8
Matthew Brabham <brabham.matthew@gmail.com>
2024-09-24 11:22:49 -0700

    Refactor db:backup commands to use Symfony ChoiceQuestion
    Replaced direct parameter inputs with Symfony's ChoiceQuestion for user-interactive application and environment selection. Added optional arguments with default values to facilitate database and backup selection processes. Improved error handling and confirmation steps for delete operations.

--- src/Commands/DbBackupCommand.php
+++ src/Commands/DbBackupCommand.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace OsuWams\Commands;
-
 
 use AcquiaCloudApi\Endpoints\DatabaseBackups;
 use Consolidation\OutputFormatters\FormatterManager;
@@ -11,6 +9,7 @@ use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
 use DateTime;
 use DateTimeZone;
 use Exception;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class DbBackupCommand extends AcquiaCommand {
 
@@ -44,39 +43,165 @@ class DbBackupCommand extends AcquiaCommand {
   /**
    * Delete a Database backup.
    *
-   * @param $appName
-   * @param $env
-   * @param $dbName
-   * @param $backupId
+   * @param array $options An array of options
+   * @option $app The Acquia Cloud Application name: prod:shortname
+   * @option $env The Environment short name: dev|prod|test
+   * @option $dbName The Database Name to get backups for.
+   * @option $backupId The ID of the backup to delete.
+   * @option $format Format the result data. Available formats:
+   *  csv,json,list,null,php,print-r,sections,string,table,tsv,var_dump,var_export,xml,yaml
+   * @option $fields Available fields: Cron ID (id), Label, (label), Command
    *
    * @command db:backup:delete
    * @throws \Exception
    */
-  public function deleteBackupDb($appName, $env, $dbName, $backupId) {
-    $appUuId = $this->getUuidFromName($appName);
-    $envUuId = $this->getEnvUuIdFromApp($appUuId, $env);
-    $this->databaseBackupAdapter->delete($envUuId, $dbName, $backupId);
+  public function deleteBackupDb(array $options = [
+    'app' => NULL,
+    'env' => NULL,
+    'dbName' => NULL,
+    'backupId' => NULL,
+    'format' => 'table',
+    'fields' => '',
+  ]
+  ) {
+    if (is_null($options['app'])) {
+      $this->say('Getting Applications...');
+      $appHelper = new ChoiceQuestion('Select which Acquia Cloud Application you want to operate on', $this->getApplicationsId());
+      $appName = $this->doAsk($appHelper);
+    }
+    else {
+      $appName = $options['app'];
+    }
+    try {
+      $appUuId = $this->getUuidFromName($appName);
+    }
+    catch (Exception $e) {
+      $this->say('Incorrect Application ID.');
+    }
+    if (is_null($options['env'])) {
+      // Get a list of environments for this App UUID.
+      $this->writeln('Getting Environment ID\'s...');
+      $envList = $this->getEnvironments($appUuId);
+      // Get the Env for the scheduled jobs.
+      $envHelper = new ChoiceQuestion('Which Environment do you want to see the domain list for...', $envList);
+      $environment = $this->doAsk($envHelper);
+    }
+    else {
+      $environment = $options['env'];
+    }
+    try {
+      $envUuId = $this->getEnvUuIdFromApp($appUuId, $environment);
+    }
+    catch (Exception $e) {
+      $this->say('Incorrect Environment and Application id.');
+    }
+    if (is_null($options['dbName'])) {
+      // Get database names.
+      $this->writeln("Getting Database Name's...");
+      $dbNames = $this->getDatabases($appUuId);
+      $dbNameHelper = new ChoiceQuestion('Which Database do you want to list backups for?', $dbNames);
+      $dbName = $this->doAsk($dbNameHelper);
+    }
+    else {
+      $dbName = $options['dbName'];
+    }
+    if (is_null($options['backupId'])) {
+      $this->writeln("Getting Backups...");
+      $backups = $this->databaseBackupAdapter->getAll($envUuId, $dbName);
+      $choices = [];
+      $backupIdMap = [];
+      /** @var \AcquiaCloudApi\Response\BackupResponse $backup */
+      foreach ($backups as $backup) {
+        $completedAt = new DateTime($backup->completedAt);
+        $completedAt->setTimezone(new DateTimeZone('America/Los_Angeles'));
+        $formattedCompletedAt = $completedAt->format('Y-m-d H:i:s');
+        $humanString = "ID: $backup->id Completed at $formattedCompletedAt, Type $backup->type";
+        $choices[] = $humanString;
+        $backupIdMap[$humanString] = $backup->id;
+      }
+      $backupIdHelper = new ChoiceQuestion('Which backup to delete?', $choices);
+      $selectedOption = $this->doAsk($backupIdHelper);
+      $backupId = $backupIdMap[$selectedOption];
+    }
+    else {
+      $backupId = $options['backupId'];
+    }
+    $makeItSo = $this->confirm("Do you want to delete this backup with the id of ${backupId}", 'y');
+    if ($makeItSo) {
+      $this->databaseBackupAdapter->delete($envUuId, $dbName, $backupId);
+    }
+    else {
+      $this->say("Aborting");
+    }
   }
 
   /**
    * List all backups for a given database.
    *
-   * @param string $appName
-   *  The Acquia CLoud Application Name.
-   * @param string $env
-   *  The Environment short name.
-   * @param string $dbName
-   *  The Database name, usually snake case.
+   * Optional arguments: app, env, dbName. If app and/or env are not provided,
+   * a helper will ask you to select from a generated list.
+   *
+   * @param array $options An array of options
+   * @option $app The Acquia Cloud Application name: prod:shortname
+   * @option $env The Environment short name: dev|prod|test
+   * @option $dbName The Database Name to get backups for.
+   * @option $format Format the result data. Available formats:
+   *  csv,json,list,null,php,print-r,sections,string,table,tsv,var_dump,var_export,xml,yaml
+   * @option $fields Available fields: Cron ID (id), Label, (label), Command
    *
    * @throws \Exception
    * @command db:backup:list
    */
-  public function listBackupDbs($appName, $env, $dbName, $options = [
+  public function listBackupDbs(array $options = [
+    'app' => NULL,
+    'env' => NULL,
+    'dbName' => NULL,
     'format' => 'table',
     'fields' => '',
-  ]) {
-    $appUuId = $this->getUuidFromName($appName);
-    $envUuId = $this->getEnvUuIdFromApp($appUuId, $env);
+  ]
+  ) {
+    if (is_null($options['app'])) {
+      $this->say('Getting Applications...');
+      $appHelper = new ChoiceQuestion('Select which Acquia Cloud Application you want to operate on', $this->getApplicationsId());
+      $appName = $this->doAsk($appHelper);
+    }
+    else {
+      $appName = $options['app'];
+    }
+    // Attempt to get the UUID of this application.
+    try {
+      $appUuId = $this->getUuidFromName($appName);
+    }
+    catch (Exception $e) {
+      $this->say('Incorrect Application ID.');
+    }
+    if (is_null($options['env'])) {
+      // Get a list of environments for this App UUID.
+      $this->writeln('Getting Environment ID\'s...');
+      $envList = $this->getEnvironments($appUuId);
+      // Get the Env for the scheduled jobs.
+      $envHelper = new ChoiceQuestion('Which Environment do you want to see the domain list for...', $envList);
+      $environment = $this->doAsk($envHelper);
+    }
+    else {
+      $environment = $options['env'];
+    }
+    try {
+      $envUuId = $this->getEnvUuIdFromApp($appUuId, $environment);
+    }
+    catch (Exception $e) {
+      $this->say('Incorrect Environment and Application id.');
+    }
+    if (is_null($options['dbName'])) {
+      // Get database names.
+      $this->writeln("Getting Database Name's...");
+      $dbNames = $this->getDatabases($appUuId);
+      $dbNameHelper = new ChoiceQuestion('Which Database do you want to list backups for?', $dbNames);
+      $dbName = $this->doAsk($dbNameHelper);
+    }
+    else {
+      $dbName = $options['dbName'];
+    }
     $backups = $this->databaseBackupAdapter->getAll($envUuId, $dbName);
     $rows = [];
     /** @var \AcquiaCloudApi\Response\BackupResponse $backup */
@@ -141,11 +266,11 @@ class DbBackupCommand extends AcquiaCommand {
     $envUuId = $this->getEnvUuIdFromApp($appUuId, $env);
     $allBackups = $this->databaseBackupAdapter->getAll($envUuId, $dbName);
     // Filter our backups to only include daily backups.
-    $dailyBackups = array_filter($allBackups->getArrayCopy(), function ($backup) {
+    $dailyBackups = array_filter($allBackups->getArrayCopy(), function($backup) {
       return $backup->type === "daily";
     });
     // Sort the backup date/time to ensure we have the latest at position 1.
-    usort($dailyBackups, function ($a, $b) {
+    usort($dailyBackups, function($a, $b) {
       return $a->completedAt < $b->completedAt;
     });
     $backupId = $dailyBackups[0]->id;
